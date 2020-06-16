3b4cea0243a90a98c7834f3f2f29100b6968a33f
Madeline Trotter <spicydonuts@gmail.com>
2020-06-16 12:09:02 -0700

    Update to react-dnd 11

--- examples/basic/src/Basic.purs
+++ examples/basic/src/Basic.purs
@@ -1,115 +1,119 @@
 module Basic where
 
 import Prelude
-
 import Data.Array ((!!), drop, mapWithIndex, take)
-import Data.Foldable (for_)
-import Data.Maybe (Maybe(Nothing), fromMaybe, maybe)
-import React.Basic (Component, JSX, StateUpdate(..), createComponent, fragment, make, runUpdate)
+import Data.Foldable (traverse_)
+import Data.Int as Int
+import Data.Maybe (fromMaybe, maybe)
+import Effect (Effect)
 import React.Basic.DOM as R
 import React.Basic.DOM.Events (targetChecked)
 import React.Basic.Events as Events
-import React.Basic.ReactDND (DragDrop, DragDropItemType(..), createDragDrop, createDragDropContext)
+import React.Basic.Hooks (Component, component, fragment, mkReducer, useReducer, (/\))
+import React.Basic.Hooks as React
+import React.Basic.ReactDND (dndProvider, mergeTargets, useDrag, useDrop)
 import React.Basic.ReactDND.Backends.HTML5Backend (html5Backend)
-
-dndContext :: JSX -> JSX
-dndContext = createDragDropContext html5Backend
-
-dnd :: DragDrop { itemId :: String, index :: Int }
-dnd = createDragDrop (DragDropItemType "TODO_ITEM")
 
 data Action
   = Move { from :: Int, to :: Int }
   | SetDone String Boolean
 
-component :: Component Unit
-component = createComponent "TodoExample"
+type Todo
+  = { id :: String, text :: String, done :: Boolean }
 
-todoExample :: JSX
-todoExample = unit # make component { initialState, render }
-  where
-    initialState =
-      { todos:
-          [ { id: "a", text: "PureScript", done: true }
-          , { id: "b", text: "React-Basic", done: true }
-          , { id: "c", text: "React-DND-Basic", done: false }
-          ]
-      }
-
-    update self = case _ of
-      Move { from, to } ->
-        Update self.state { todos = moveItem from to self.state.todos }
-
-      SetDone id done ->
-        Update self.state
-          { todos = self.state.todos <#> \t ->
-              if t.id == id
-              then t { done = done }
-              else t
-          }
-
-    send self = runUpdate update self
-
-    render self =
-      dndContext $
-        fragment
+mkTodoExample :: Component Unit
+mkTodoExample = do
+  todo <- mkTodo
+  reducer <- mkReducer update
+  React.component "TodoExample" \_ -> React.do
+    state /\ dispatch <- useReducer initialState reducer
+    pure
+      $ dndProvider html5Backend
+      $ fragment
           [ R.h1_ [ R.text "Todos" ]
           , R.p_ [ R.text "Drag to reorder the list:" ]
-          , R.section_ (mapWithIndex renderTodo self.state.todos)
+          , R.section_
+              $ state.todos
+              # mapWithIndex \index t -> todo { index, todo: t, dispatch }
           ]
+  where
+  initialState =
+    { todos:
+        [ { id: "a", text: "PureScript", done: true }
+        , { id: "b", text: "React Basic", done: true }
+        , { id: "c", text: "React Basic DND", done: false }
+        ]
+    }
 
-      where
-        renderTodo index todo =
-          dnd.dragSource
-            { beginDrag: \_ -> pure
-                { itemId: todo.id
-                , index
+  update state = case _ of
+    Move { from, to } ->
+      state
+        { todos = moveItem from to state.todos
+        }
+    SetDone id done ->
+      state
+        { todos =
+          state.todos
+            <#> \t ->
+                if t.id == id then
+                  t { done = done }
+                else
+                  t
+        }
+
+mkTodo ::
+  Component
+    { index :: Int
+    , todo :: Todo
+    , dispatch :: Action -> Effect Unit
+    }
+mkTodo = do
+  let
+    todoDND = "todo-dnd"
+  component "Todo" \{ index, todo, dispatch } -> React.do
+    { isDragging, connectDrag } <- useDrag { type: todoDND, id: show index }
+    { id: maybeDragItem, isOver, connectDrop } <-
+      useDrop
+        { accept: todoDND
+        , onDrop: Int.fromString >>> traverse_ \id -> dispatch $ Move { from: id, to: index }
+        }
+    pure
+      $ R.label
+          { ref: mergeTargets connectDrag connectDrop
+          , style:
+              R.css
+                { display: "block"
+                , padding: "0.3rem 0.8rem"
+                , alignItems: "center"
+                , borderTop:
+                    if isOver && maybe false ((_ > show index)) maybeDragItem then
+                      "0.2rem solid #0044e4"
+                    else
+                      "0.2rem solid transparent"
+                , borderBottom:
+                    if isOver && maybe false ((_ < show index)) maybeDragItem then
+                      "0.2rem solid #0044e4"
+                    else
+                      "0.2rem solid transparent"
+                , opacity: if isDragging then 0.1 else 1.0
                 }
-            , endDrag: const (pure unit)
-            , canDrag: const (pure true)
-            , isDragging: \{ item: draggingItem } ->
-                pure $ maybe false (\i -> i.itemId == todo.id) draggingItem
-            , render: \{ connectDragSource, isDragging } ->
-                dnd.dropTarget
-                  { drop: \{ item: dragItem } -> do
-                      for_ (_.index <$> dragItem) \dragItemIndex ->
-                        send self $ Move { from: dragItemIndex, to: index }
-                      pure Nothing
-                  , hover: const (pure unit)
-                  , canDrop: const (pure true)
-                  , render: \{ connectDropTarget, isOver, item: maybeDragItem } ->
-                      connectDragSource $ connectDropTarget $
-                        R.div
-                          { style: R.css
-                              { padding: "0.3rem 0.8rem"
-                              , alignItems: "center"
-                              , borderTop:
-                                  if isOver && (fromMaybe false ((\dragItem -> dragItem.index > index) <$> maybeDragItem))
-                                  then "0.2rem solid #0044e4"
-                                  else "0.2rem solid transparent"
-                              , borderBottom:
-                                  if isOver && (fromMaybe false ((\dragItem -> dragItem.index < index) <$> maybeDragItem))
-                                  then "0.2rem solid #0044e4"
-                                  else "0.2rem solid transparent"
-                              , opacity: if isDragging then 0.1 else 1.0
-                              }
-                          , children:
-                              [ R.input
-                                  { "type": "checkbox"
-                                  , checked: todo.done
-                                  , onChange: Events.handler targetChecked \checked -> do
-                                      send self $ SetDone todo.id $ fromMaybe false checked
-                                  }
-                              , R.text todo.text
-                              ]
-                          }
+          , children:
+              [ R.input
+                  { type: "checkbox"
+                  , checked: todo.done
+                  , onChange:
+                      Events.handler targetChecked \checked -> do
+                        dispatch $ SetDone todo.id $ fromMaybe false checked
                   }
-            }
+              , R.text todo.text
+              ]
+          }
 
 moveItem :: forall a. Int -> Int -> Array a -> Array a
 moveItem fromIndex toIndex items =
   let
     item = items !! fromIndex
+
     items' = take fromIndex items <> drop (fromIndex + 1) items
   in
     take toIndex items'
--- src/React/Basic/ReactDND.purs
+++ src/React/Basic/ReactDND.purs
@@ -1,147 +1,87 @@
 module React.Basic.ReactDND
   ( Backend
-  , DragDropItemType(..)
-  , Coords
-  , SharedCollectArgs
-  , DragSourceCollectArgs
-  , DropTargetCollectArgs
-  , DragLayerCollectArgs
-  , DragSourceProps
-  , DropTargetProps
-  , DragLayerProps
-  , DragDrop
-  , createDragDropContext
-  , createDragDrop
+  , DNDContext
+  , dndProvider
+  , useDrag
+  , UseDrag
+  , useDrop
+  , UseDrop
+  , mergeTargets
   ) where
 
 import Prelude
-
-import Data.Function.Uncurried (Fn2, mkFn1, runFn2)
 import Data.Maybe (Maybe)
-import Data.Nullable (Nullable, toMaybe, toNullable)
+import Data.Nullable (Nullable)
+import Data.Nullable as Nullable
 import Effect (Effect)
-import Effect.Uncurried (mkEffectFn1)
-import React.Basic (JSX, ReactComponent, element)
+import Effect.Uncurried (EffectFn1, runEffectFn1)
+import React.Basic.Hooks (Hook, JSX, ReactComponent, Ref, element, unsafeHook)
+import Web.DOM (Node)
 
-data Backend
+foreign import data Backend :: Type
 
-newtype DragDropItemType = DragDropItemType String
+foreign import data DNDContext :: Type
 
-type Coords = { x :: Number, y :: Number }
+dndProvider :: Backend -> JSX -> JSX
+dndProvider backend children = element dndProvider_ { backend, children }
 
-type SharedCollectArgs item =
-  ( itemType :: Maybe DragDropItemType
-  , item :: Maybe item
-  , initialClientOffset :: Maybe Coords
-  , initialSourceClientOffset :: Maybe Coords
-  , clientOffset :: Maybe Coords
-  , differenceFromInitialOffset :: Maybe Coords
-  , sourceClientOffset :: Maybe Coords
-  )
+type Coords
+  = { x :: Number, y :: Number }
 
-type DragSourceCollectArgs item =
-  { connectDragSource :: JSX -> JSX
-  , connectDragPreview :: JSX -> JSX
-  , canDrag :: Maybe Boolean
-  , isDragging :: Boolean
-  , dropResult :: Maybe item
-  , didDrop :: Boolean
-  | SharedCollectArgs item
-  }
+useDrag ::
+  { type :: String
+  , id :: String
+  } ->
+  Hook UseDrag
+    { isDragging :: Boolean
+    , connectDrag :: Ref (Nullable Node)
+    }
+useDrag item =
+  unsafeHook do
+    runEffectFn1 useDrag_
+      { item
+      , collect: Nullable.null
+      }
 
-type DropTargetCollectArgs item =
-  { connectDropTarget :: JSX -> JSX
-  , canDrop :: Boolean
-  , isOver :: Boolean
-  , isOverShallow :: Boolean
-  , dropResult :: Maybe item
-  , didDrop :: Boolean
-  | SharedCollectArgs item
-  }
+foreign import data UseDrag :: Type -> Type
 
-type DragLayerCollectArgs item =
-  { isDragging :: Boolean
-  | SharedCollectArgs item
-  }
+useDrop ::
+  { accept :: String
+  , onDrop :: String -> Effect Unit
+  } ->
+  Hook UseDrop
+    { id :: Maybe String
+    , isOver :: Boolean
+    , connectDrop :: Ref (Nullable Node)
+    }
+useDrop opts =
+  unsafeHook do
+    { id, isOver, connectDrop } <-
+      runEffectFn1 useDrop_ opts
+    pure { id: Nullable.toMaybe id, isOver, connectDrop }
 
-type DragSourceProps item =
-  { beginDrag :: DragSourceCollectArgs item -> Effect item
-  , endDrag :: DragSourceCollectArgs item -> Effect Unit
-  , canDrag :: DragSourceCollectArgs item -> Effect Boolean
-  , isDragging :: DragSourceCollectArgs item -> Effect Boolean
-  , render :: DragSourceCollectArgs item -> JSX
-  }
+foreign import data UseDrop :: Type -> Type
 
-type DropTargetProps item =
-  { drop :: DropTargetCollectArgs item -> Effect (Maybe item)
-  , hover :: DropTargetCollectArgs item -> Effect Unit
-  , canDrop :: DropTargetCollectArgs item -> Effect Boolean
-  , render :: DropTargetCollectArgs item -> JSX
-  }
+foreign import dndProvider_ :: ReactComponent { backend :: Backend, children :: JSX }
 
-type DragLayerProps item =
-  { render :: DragLayerCollectArgs item -> JSX
-  }
+foreign import useDrag_ ::
+  forall props.
+  EffectFn1
+    { item :: { type :: String, id :: String }
+    , collect :: Nullable (Unit -> props)
+    }
+    { isDragging :: Boolean
+    , connectDrag :: Ref (Nullable Node)
+    }
 
-type DragDrop item =
-  { dragSource :: DragSourceProps item -> JSX
-  , dropTarget :: DropTargetProps item -> JSX
-  , dragLayer :: DragLayerProps item -> JSX
-  }
+foreign import useDrop_ ::
+  EffectFn1
+    { accept :: String
+    , onDrop :: String -> Effect Unit
+    }
+    { id :: Nullable String
+    , isOver :: Boolean
+    , connectDrop :: Ref (Nullable Node)
+    }
 
-createDragDrop
-  :: forall item
-   . DragDropItemType
-  -> DragDrop { | item }
-createDragDrop itemType =
-  { dragSource
-  , dropTarget
-  , dragLayer
-  }
-  where
-    dragSource =
-      let jsDragSource = runFn2 unsafeCreateDragSource toMaybe itemType
-      in \props -> element jsDragSource
-            { beginDrag: mkEffectFn1 props.beginDrag
-            , endDrag: mkEffectFn1 props.endDrag
-            , canDrag: mkEffectFn1 props.canDrag
-            , isDragging: mkEffectFn1 props.isDragging
-            , render: mkFn1 props.render
-            }
-
-    dropTarget =
-      let jsDropTarget = runFn2 unsafeCreateDropTarget toMaybe itemType
-      in \props -> element jsDropTarget
-            { drop: mkEffectFn1 (map toNullable <<< props.drop)
-            , hover: mkEffectFn1 props.hover
-            , canDrop: mkEffectFn1 props.canDrop
-            , render: mkFn1 props.render
-            }
-
-    dragLayer =
-      let jsDragLayer = unsafeCreateDragLayer toMaybe
-      in element jsDragLayer
-
-createDragDropContext :: Backend -> JSX -> JSX
-createDragDropContext backend = (\c child -> element c { child }) (createDragDropContext_ backend)
-
-foreign import createDragDropContext_ :: Backend -> ReactComponent { child :: JSX }
-
-foreign import unsafeCreateDragSource
-  :: forall a
-   . Fn2
-      (Nullable ~> Maybe)
-      DragDropItemType
-      (ReactComponent { | a })
-
-foreign import unsafeCreateDropTarget
-  :: forall a
-   . Fn2
-      (Nullable ~> Maybe)
-      DragDropItemType
-      (ReactComponent { | a })
-
-foreign import unsafeCreateDragLayer
-  :: forall a
-   . (Nullable ~> Maybe)
-  -> ReactComponent { | a }
+foreign import mergeTargets :: Ref (Nullable Node) -> Ref (Nullable Node) -> Ref (Nullable Node)
