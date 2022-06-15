8bff7b1a2a0605de6e662255002ed715aa84e7cc
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2022-06-15 10:36:34 -0400

    Remove examples/basic/index.js

--- examples/basic/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const ReactDOM = require("react-dom");
-const TodoExample = require("./output/bundle.js");
-
-ReactDOM.render(
-  TodoExample.mkTodoExample()(),
-  document.getElementById("container")
-);
